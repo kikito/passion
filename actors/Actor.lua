6a2e4b35deb16f12aefe3b16d5f8091201450b4b
kikito <kikito@gmail.com>
2009-12-27 22:13:59 +0000

    Started using getters and setters on Actor.lua

--- actors/Actor.lua
+++ actors/Actor.lua
@@ -34,38 +34,26 @@ end
 
 function passion.Actor:update(dt) end
 
-function passion.Actor:getX() return self.x end
-function passion.Actor:getY() return self.y end
+passion.Actor:getterSetter('x') --getX, setX
+passion.Actor:getterSetter('y') -- getY, setY
+passion.Actor:getterSetter('angle') -- getAngle, setAngle
+passion.Actor:getterSetter('centerX') -- getCenterX, setCenterX
+passion.Actor:getterSetter('centerY') -- getCenterY, setCenterY
+passion.Actor:getterSetter('scaleX', 1) -- getScaleX, setScaleX, with 1 as default value
+passion.Actor:getterSetter('scaleY', 1) --getScaleY, setScaleY, with 1 as defalult value
+
 function passion.Actor:getPosition() return self.x, self.y end
-
-function passion.Actor:getAngle() return self.angle end
-
-function passion.Actor:getCenterX() return self.centerX or 0 end
-function passion.Actor:getCenterY() return self.centerY or 0 end
-function passion.Actor:getCenter() return {self.centerX or 0, self.centerX or 0} end
-
-function passion.Actor:getScaleX() return self.scaleX or 1 end
-function passion.Actor:getScaleY() return self.scaleY or 1 end
-
-
-function passion.Actor:setX(x) self.x = x end
-function passion.Actor:setY(y) self.y = y end
 function passion.Actor:setPosition(x, y)
   self.x = x
   self.y = y
 end
 
-function passion.Actor:setAngle(angle) self.angle = angle end
-
-function passion.Actor:setCenterX(centerX) self.centerX = centerX end
-function passion.Actor:setCenterY(centerY) self.centerY = centerY end
+function passion.Actor:getCenter() return self.centerX, self.centerX end
 function passion.Actor:setCenter(centerX, centerY)
   self.centerX = centerX
   self.centerY = centerY
 end
 
-function passion.Actor:setScaleX(scaleX) self.scaleX = scaleX end
-function passion.Actor:setScaleY(scaleY) self.scaleY = scaleY end
 function passion.Actor:setScale(scale)
   self.scaleX = scale
   self.scaleY = scale
