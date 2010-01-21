4823008abc43acf8fbc8aa246834f45058fadbdf
kikito <kikito@gmail.com>
2010-01-21 19:19:21 +0000

    Several improvements on gui/Label.lua

--- gui/Label.lua
+++ gui/Label.lua
@@ -1,53 +1,69 @@
 require 'passion.gui.Pannel'
+require 'passion.ResourceManager'
 
 passion.gui.Label = class('passion.gui.Label', passion.gui.Pannel)
+local Label = passion.gui.Label
 
-passion.gui.Label.VALID_OPTIONS = {'text', 'font', 'fontColor', 'fontSize', 'align'}
+--obtain the default font
+love.graphics.setFont(12)
+local defaultFont = love.graphics.getFont()
 
-function passion.gui.Label:initialize(options)
+local VALID_OPTIONS = {'text', 'font', 'fontColor', 'align'}
+
+function Label:initialize(options)
   super(self, options)
-  self:parseOptions(options, passion.gui.Label.VALID_OPTIONS)
+  self:parseOptions(options, VALID_OPTIONS)
 end
 
-passion.gui.Label:getterSetter('text','')
-passion.gui.Label:getterSetter('font')
-passion.gui.Label:getterSetter('fontColor', passion.white)
-passion.gui.Label:getterSetter('fontSize', 12)
-passion.gui.Label:getterSetter('align')
-passion.gui.Label:getterSetter('borderColor', nil)
+Label:getterSetter('text',       '')
+Label:getterSetter('font',       defaultFont)
+Label:getterSetter('fontColor',  passion.white)
+Label:getterSetter('align',      'left')
+Label:getterSetter('borderColor', nil)
 
 -- FIXME: setText should update width
 -- FIXME: setFontSize should update height  ?
 
-function passion.gui.Label:getHeight()
+-- FIXME: review this
+function Label:getHeight()
   return super(self) or 2*self:getPadding() + self:getFontSize()
 end
 
-function passion.gui.Label:draw()
+function Label:getFontSize()
+  local f = self:getFont()
+  return f:getHeight()
+end
+
+function Label:draw()
 
   local x, y = self:getPosition()
   local align = self:getAlign()
-  local fontColor = self:getFontColor()
   local font = self:getFont()
+  local fontColor = self:getFontColor()
   local fontSize = self:getFontSize()
   local width = self:getWidth()
   local text = self:getText()
   local padding = self:getPadding()
 
-  if(font ~= nil and fontSize ~= nil) then love.graphics.setFont(font, fontSize)
-  elseif(fontSize ~= nil) then
-    love.graphics.setFont(fontSize)
-    font = love.graphics.getFont()
-  end
-  
+
+  local prevFont = love.graphics.getFont()
+  local pr,pg,pb,pa = love.graphics.getColor() -- previous font color
+
+  super(self) -- draws background and borders using the Pannel implementation
+
+  love.graphics.setFont(font)
+
   if(width == nil and font ~= nil) then width = font:getWidth(text) end
-  
-  super(self)
 
   if(fontColor~=nil) then love.graphics.setColor(unpack(fontColor)) end
 
-  if(align==nil) then love.graphics.printf(text, x+padding, y+fontSize+padding-1, width)
+  if    (align=='left') then love.graphics.printf(text, x+padding, y+fontSize+padding-1, width, 'left')
   elseif(align=='center') then love.graphics.printf(text, x, y+fontSize+padding-1, width, 'center')
-  else love.graphics.printf(text, x+padding, y+fontSize+padding-1, width, align)
+  else love.graphics.printf(text, x, y+fontSize+padding-1, width-padding, 'right')
   end
+
+  -- restore previous values of font & color
+  if(prevFont~=nil) then love.graphics.setFont(prevFont) end
+  love.graphics.setColor(pr, pg, pb, pa)
+
 end
