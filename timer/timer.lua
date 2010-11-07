7e1368ff19778e9f31ea6ea26d6fcd636abae044
Enrique García <kikito@gmail.com>
2010-11-07 23:02:04 +0100

    small change on timer.lua

--- timer/timer.lua
+++ timer/timer.lua
@@ -7,7 +7,7 @@ module('passion.timer')
 
 -- These functions encapsulate the timer creation so it is transparent to the user.
 
-function after(seconds, f, ...)
+function later(seconds, f, ...)
   return _G.passion.timer.Timer:new(seconds, f, ...)
 end
 
@@ -18,7 +18,6 @@ end
 function effect(object, seconds, properties, easing, callback, ...)
   return _G.passion.timer.Effect:new(object, seconds, properties, easing, callback, ...)
 end
-
 
 function update(dt)
   _G.passion.timer.Timer:apply('update', dt)
